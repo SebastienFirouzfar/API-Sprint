f2b460f58780227e2a21c2f4dc86d5982c932c86
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-16 16:58:20 +0100

    Modification erreur

--- exoAPI.js
+++ exoAPI.js
@@ -1,11 +1,16 @@
 //Exercice fetch 
+let paragraphe = document.createElement("p")
+//let buttonAPi = document.getElementById("buttonUser")
 document.querySelector("#buttonUser").addEventListener("click", function () {
-    fetch("http://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json())
         .then((data) => {
             data.forEach(element => {
-                document.getElementById("buttonUser").insertAdjacentHTML("beforebegin",'<br><br><p>"'+element.name+'"</p>')
-                console.log(data)
+               // buttonAPi.insertAdjacentHTML("beforebegin",'<br><br><p>"'+element.name+'"</p>' )
+               paragraphe.append('\n'+ element.name +"  "+ element.username + "   "+ element.email+"\u0020")
+               paragraphe.setAttribute('style', 'margin : 10px ; padding : 10px;')
+               console.log(data)
             });
         })
+        document.body.appendChild(paragraphe)
 })
