46cc2da4f31eb6d60a6ff46741bc9785035adc41
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-30 22:06:57 +0100

    Modification de la structure

--- exoAPI.js
+++ exoAPI.js
@@ -1,18 +1,23 @@
 //Exercice fetch 
 let paragraphe = document.createElement("p")
-//let buttonAPi = document.getElementById("buttonUser")
 document.querySelector("#buttonUser").addEventListener("click", function () {
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json())
         .then((data) => {
             data.forEach(element => {
-               // buttonAPi.insertAdjacentHTML("beforebegin",'<br><br><p>"'+element.name+'"</p>' )
-               paragraphe.append('\n'+ element.name +"  "+ element.username + "   "+ element.email+"\u0020")
+              // buttonAPi.insertAdjacentHTML("beforebegin",'<br><br><p>"'+element.name+'"</p>' )
+               paragraphe.insertAdjacentHTML("beforebegin",' <p>"'+element.name +'"</p> ' )
+               paragraphe.insertAdjacentHTML("beforebegin",' <p>"'+element.username +'"</p> ' )
+               paragraphe.insertAdjacentHTML("beforebegin",' <p>"'+element.email +'"</p> ' )
+               paragraphe.insertAdjacentHTML("beforebegin",'<br> <br>' )
+               //paragraphe.append(element.name) 
+               //paragraphe.append(element.username )
+               //paragraphe.append(element.email )
                paragraphe.setAttribute('style', 'margin : 10px ; padding : 10px;')
                console.log(data)
             });
         })
         //afficher les données tel que le nom, prenom et email dans l'html
-        //fin exercice api
+        //fin exercice
         document.body.appendChild(paragraphe)
 })
